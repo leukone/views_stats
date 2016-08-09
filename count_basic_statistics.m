e000d79acace0904358bf1705f9922aaeee3958a
leukone <a.m.scisly@student.uw.edu.pl>
2016-08-09 22:59:55 +0200

    Delete count_basic_statistics.m

--- count_basic_statistics.m
+++
@@ -1,9 +0,0 @@
-function x = count_basic_statistics (data, n)
-  range = max(data)-min(data);
-  std_v = std(data);
-  var_v = var(data);
-  mean_v = mean(data);
-  max_v = max(data);
-  min_v = min(data);
-  x = [n, range, mean_v, std_v, var_v, min_v, max_v];
-end
