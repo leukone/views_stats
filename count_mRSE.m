828defd9e8634cd630005328b8d81391f231a7d8
leukone <a.m.scisly@student.uw.edu.pl>
2016-08-10 02:02:30 +0200

    Delete count_mRSE.m

--- count_mRSE.m
+++
@@ -1,7 +0,0 @@
-function prediction = count_prediction(input, output)
-    samples = length(input(:,1));
-    %design matrix
-    dm = [ones(samples, 1) input];
-    beta = inv(dm'*dm)*dm'*output;
-    prediction = dm*beta;
-end
