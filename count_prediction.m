28458be359b0f40fa7d45c0e8293c23798989bd6
leukone <a.m.scisly@student.uw.edu.pl>
2016-08-13 21:59:25 +0200

    Update count_prediction.m

--- count_prediction.m
+++ count_prediction.m
@@ -2,8 +2,6 @@ function [prediction beta] = count_prediction (input, output, beta)
     samples = length(input(:,1));
     %design matrix
     dm = [ones(samples, 1) input];
-    reg_matrix = eye(length(dm(1,:)));
-    reg_matrix(1,1) = 0;
     if beta == false
         beta = inv(dm'*dm)*dm'*output;
     end
